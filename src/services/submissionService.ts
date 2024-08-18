547190b0200dc383db5a16cf7aaf975cca262b0e
Jeffery05 <jefferyhu2005@gmail.com>
2024-08-17 21:59:06 -0400

    Added a way to get the correct image from submissionService

--- src/services/submissionService.ts
+++ src/services/submissionService.ts
@@ -4,6 +4,16 @@ import { prisma } from "../util/prismaClient";
 class SubmissionService {
   async createSubmission(userid: number, challengeid: number, attemptedimageid: string): Promise<any> {
     console.log(challengeid)
+    const challengeImageID = await prisma.challenge.findUnique({
+      where: {
+        id: challengeid,
+      },
+      select: {
+        correctImage: true,
+      },
+    });
+    console.log(challengeImageID)
+
     const newSubmission = await prisma.submission.create({
       data: {
         isCorrect: true,
